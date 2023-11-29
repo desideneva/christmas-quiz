4bd221015b78187d0f52f3d44d93e358b9a5b995
Desislava Deneva <tolteki3@abv.bg>
2023-11-29 23:13:37 +0000

    Change styling to the headings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
- 
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("playButton").addEventListener("click", function () {
+        // Add your logic to start the quiz when the button is clicked
+        alert("Quiz started! Add your quiz logic here.");
+    });
+});
