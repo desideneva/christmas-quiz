3297be3cdb9b6ccdfe2053c945f8406cc8809c53
Desislava Deneva <tolteki3@abv.bg>
2023-12-03 21:17:17 +0000

    Create script.js file and add it to assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -153,16 +153,16 @@ function showScore() {
     resetState();
 
     // Set background image or color for the score screen
-    document.body.style.backgroundImage = 'url("assets/images/merry-christmas.jpg")';
+    document.body.style.backgroundImage = 'url("assets/images/merry-christmas.png")';
 
     // Update the question element with the score
     if (score === questions.length) {
         // Display a congratulatory message for a perfect score
         questionElement.innerHTML = "Wow... 7 out of 7! Congratulations! You made it!";
     } else {
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = "Play Again";
-    nextButton.style.display = "block";
+        questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+        nextButton.innerHTML = "Play Again";
+        nextButton.style.display = "block";
     }
 }
 
