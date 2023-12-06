e9b3c4deaf2ab24221a1b57052dc776563402748
Desislava Deneva <tolteki3@abv.bg>
2023-12-06 16:15:58 +0000

    Add rules in popup alert message

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,9 +5,24 @@ document.addEventListener("DOMContentLoaded", function () {
         // Navigate to the quiz.html page
         window.location.href = 'quiz.html';
     });   
-        popupButton.addEventListener("click", function () {
-            // Show a popup message
-            alert("This is a popup message!");
+        rulesButton.addEventListener("click", function () {
+            // Display the rules as bullet points in an alert
+        const rules = [
+            "  - Click the 'PLAY' button to begin the quiz.",
+            "  - Read each question carefully.",
+            "  - Select an answer by clicking on one of the options.",
+            "  - If the correct answer is chosen, it will be highlighted in green.",
+            "  - If the wrong answer is chosen, it will be highlighted in red, and the correct answer will be highlighted in green.",
+            "  - You have only one attempt to select an answer.",
+            "  - Continue to the next question by clicking the 'Next Question' button.",
+            "  - To exit the quiz, click the 'EXIT' button.",
+            "  - Have fun! :)"
+        ];
+        // Combine the rules into a single string with line breaks
+        const rulesMessage = rules.join('\n');
+
+        // Show a popup message with the rules
+        alert(rulesMessage);
     });
 });
 
