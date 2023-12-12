ae851b1d43cb9b61144e359c0ea2c0d8043db667
Desislava Deneva <tolteki3@abv.bg>
2023-12-12 20:59:44 +0000

    Add modal box for the quiz rules

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
     const playButton = document.getElementById('playButton');
     const rulesButton = document.getElementById("rulesButton");
-    const rulesContainer = document.getElementById("rulesContainer");
-
+     
     playButton.addEventListener('click', function () {
         // Navigate to the quiz.html page
         window.location.href = 'quiz.html';
     });
-    
-    rulesButton.addEventListener("click", function () {
-        // Toggle the display of rules
-        if (rulesContainer.style.display === "none" || rulesContainer.style.display === "") {
-            rulesContainer.style.display = "block";
-        } else {
-            rulesContainer.style.display = "none";
-        }
-    });
 });
+ // Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("rulesButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
