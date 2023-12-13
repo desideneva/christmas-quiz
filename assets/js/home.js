dfc6561c7719a4544d99a738894cef496d8e1dd4
Desislava Deneva <tolteki3@abv.bg>
2023-12-13 14:00:26 +0000

    Update styling of front page

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const playButton = document.getElementById('playButton');
-    const rulesButton = document.getElementById("rulesButton");
-     
-    playButton.addEventListener('click', function () {
-        // Navigate to the quiz.html page
-        window.location.href = 'quiz.html';
-    });
+  const playButton = document.getElementById('playButton');
+
+  playButton.addEventListener('click', function () {
+    // Navigate to the quiz.html page
+    window.location.href = 'quiz.html';
+  });
 });
- // Get the modal
+// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -17,19 +16,19 @@ var btn = document.getElementById("rulesButton");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
- 
+};
+
